d23d847c6979e4d28eea14999323999c02bbc242
UnSk1LLe <pashatanki222@gmail.com>
2023-06-14 09:39:04 +0600

    final

---
+++ api_service/app/pkg/handlers/metric/handler.go
@@ -0,0 +1,23 @@
+package metric
+
+import (
+	"api_service/pkg/logging"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+const (
+	URL = "/api/heartbeat"
+)
+
+type Handler struct {
+	Logger logging.Logger
+}
+
+func (h *Handler) Register(router *httprouter.Router) {
+	router.HandlerFunc(http.MethodGet, URL, h.Heartbeat)
+}
+
+func (h *Handler) Heartbeat(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(204)
+}
---
+++ tag_service/app/pkg/handlers/metric/handler.go
@@ -0,0 +1,23 @@
+package metric
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"tag_service/pkg/logging"
+)
+
+const (
+	URL = "/api/heartbeat"
+)
+
+type Handler struct {
+	Logger logging.Logger
+}
+
+func (h *Handler) Register(router *httprouter.Router) {
+	router.HandlerFunc(http.MethodGet, URL, h.Heartbeat)
+}
+
+func (h *Handler) Heartbeat(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(204)
+}
